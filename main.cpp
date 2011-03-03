4bea529c396eebe6e196bcfab80507b664b9afc3
Michael Riedlin <mriedlin@gmail.com>
2011-03-03 14:22:51 -0800

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,46 @@
-//#include "element.h"
+/* Michael Riedlin
+   Karthik Handady
+   Main for hw4 */
+
+//include statements
+#include "dict.cpp"
 #include "word.cpp"
+//#include "phrase.cpp"
+//include "sentence.cpp"
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int main(){
-int i=0;
-int flag=0;
-Word word;
-string input;
-cout<<"Enter a partial word, phrase, or sentence: ";
-getline(cin, input);								//get input
-for(i=0; i < input.length(); i++)					//flags if it is word, phrase, or sentence
+int main()
 {
-	if(input[i] == '.'){							
-		flag = 3;
+	const Dict book("new-data.txt");
+	char quit = "n";
+	while(quit != "y")						//main loop
+	{
+		int i=0;
+		int flag=0;
+		string input;
+		cout<<"Enter a partial word, phrase, or sentence: ";
+		getline(cin, input);					//get input
+		for(i=0; i < input.length(); i++)			//determine input type
+		{
+			if(input[i] == '.'){							
+				flag = 3;
+			}
+			else if(input[i] == '\t'){
+				flag = 1;
+			}
+			else
+				flag = 2;
+			}
+		if(flag == 1){               				//if input was flagged as word
+			Word *word = new word;
+			word->complete(book);
+			word->correction(book);
+			word->show();
+			cout << "Enter y to quit" << endl;
+			cin >> quit;
+		}
 	}
-	else if(input[i] == '\t'){
-		flag = 1;
-	}
-	else
-		flag = 2;
 }
-if(flag == 1){               //if input was flagged as word
-	word.complete(input);
-	word.correction(input);
-}}
